7b710a6dc00e504b51ddadf946e94346cfe62464
Robert <robert.warmack@gmail.com>
2017-10-21 13:13:15 -0500

    Removing routes I override.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Payola::Engine.routes.draw do
   match '/subscribe/:plan_class/:plan_id' => 'subscriptions#create',   via: :post,   as: :subscribe
   match '/confirm_subscription/:guid'     => 'subscriptions#show',     via: :get,    as: :confirm_subscription
   match '/subscription_status/:guid'      => 'subscriptions#status',   via: :get,    as: :subscription_status
-  match '/cancel_subscription/:guid'      => 'subscriptions#destroy',  via: :delete, as: :cancel_subscription
-  match '/change_plan/:guid'              => 'subscriptions#change_plan', via: :post, as: :change_subscription_plan
+#  match '/cancel_subscription/:guid'      => 'subscriptions#destroy',  via: :delete, as: :cancel_subscription
+#  match '/change_plan/:guid'              => 'subscriptions#change_plan', via: :post, as: :change_subscription_plan
   match '/change_quantity/:guid'          => 'subscriptions#change_quantity', via: :post, as: :change_subscription_quantity
   match '/update_card/:guid'              => 'subscriptions#update_card', via: :post, as: :update_card
 
