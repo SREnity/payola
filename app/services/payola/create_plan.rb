56bc82f54e65f6daa3b527f123ff75b49548d772
Robert <robert.warmack@gmail.com>
2019-05-05 16:36:56 -0700

    Create plan changes.

--- app/services/payola/create_plan.rb
+++ app/services/payola/create_plan.rb
@@ -14,6 +14,8 @@ module Payola
         amount:            plan.amount,
         interval:          plan.interval,
         name:              plan.name,
+        aggregate_usage:   plan.respond_to?(:aggregate_usage) ? plan.aggregate_usage : nil, 
+        usage_type:        plan.respond_to?(:usage_type) ? plan.usage_type : nil,
         interval_count:    plan.respond_to?(:interval_count) ? plan.interval_count : nil,
         currency:          plan.respond_to?(:currency) ? plan.currency : Payola.default_currency,
         trial_period_days: plan.respond_to?(:trial_period_days) ? plan.trial_period_days : nil
